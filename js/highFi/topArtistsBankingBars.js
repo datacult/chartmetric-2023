9f9f89da2de4bfd27880e2f1061a361c707db313
tututwo <tugordon@outlook.com>
2024-01-12 13:20:56 -0500

    Test: Top Artists ranking bars

--- js/highFi/topArtistsBankingBars.js
+++ js/highFi/topArtistsBankingBars.js
@@ -1,47 +1,18 @@
 import { chartDimensions } from "../chartDimensions.js";
-async function draw() {
-  // parameters
-  let dataUrl = "./data/A2-12.csv";
-  let chartContainerId = "topArtistsBankingBars_barChart";
-  let widthKey = "CM_SCORE";
-  let selectedValue = "United States";
+
+export async function draw(dataUrl, chartContainerId, widthKey, selectedValue) {
   /***********************
    *1. Access data
    ************************/
 
-  let dataset = await d3.csv(dataUrl);
-  dataset.forEach((d) => {
-    d.CM_SCORE = +d.CM_SCORE;
-  });
+  let dataset = await d3.csv(dataUrl, d3.autoType);
+
   dataset = dataset.sort((a, b) => d3.descending(a[widthKey], b[widthKey])); // .sort((a, b) => d3.descending(a.SPINS, b.SPINS));
   let countries_names = [...new Set(dataset.map((d) => d.COUNTRY_NAME))];
   countries_names.unshift("All Countries");
   // let data = dataset.filter((d) => d.COUNTRY_NAME == selectedValue);
   let data = dataset.slice(0, 10);
 
-  // handle dropdown
-  const dropdown = d3.select("#topArtistsBankingBars_countrydropdown");
-  dropdown
-    .select("select")
-    .selectAll("option")
-    .data(countries_names)
-    .join("option")
-    .attr("value", (d) => d)
-    .text((d) => d);
-  dropdown.on("change", onDropdownChange);
-  function onDropdownChange(event) {
-    // Access the selected value directly from the event object
-    selectedValue = event.target.value;
-
-    if (selectedValue == "All Countries") {
-      data = dataset.slice(0, 10);
-      drawCanvas(data);
-    } else {
-      data = dataset.filter((d) => d.COUNTRY_NAME == selectedValue);
-
-      drawCanvas(data);
-    }
-  }
   /***********************
    *2. Create chart dimensions
    ************************/
@@ -56,15 +27,14 @@ async function draw() {
 
   // Select the top 10 rows using d3.slice
 
-  const top10 = data.slice(0, 10);
+  const globalTop10 = data.slice(0, 10);
 
-  // console.log(data.filter((d) => d.COUNTRY_NAME == "United States")
   /***********************
    *4. Create scales
    ************************/
   const widthScale = d3
     .scaleLinear()
-    .domain([0, d3.max(top10, (d) => d[widthKey])])
+    .domain([0, d3.max(globalTop10, (d) => d[widthKey])])
     .range([350, width]);
 
   /***********************
@@ -118,11 +88,12 @@ async function draw() {
     );
   }
 
-  drawCanvas(top10);
-  //  .style("background", "steelblue")
-  //  .style("padding", "3px")
-  //  .style("margin", "1px")
-  //  .style("width", (d,i) => widthScale(d[widthKey]) + "px")
-}
+  if (selectedValue == "All Countries") {
+    data = dataset.slice(0, 10);
+    drawCanvas(data);
+  } else {
+    data = dataset.filter((d) => d.COUNTRY_NAME == selectedValue);
 
-draw();
+    drawCanvas(data);
+  }
+}
