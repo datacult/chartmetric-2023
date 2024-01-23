ae7fbe34170f32dd3eb39c018a7613e0c3625f41
neil-oliver <neiloliverphoto@gmail.com>
2024-01-23 10:10:52 -0500

    moved options into visuals object

--- js/webflow.js
+++ js/webflow.js
@@ -7,13 +7,22 @@
     // hold data and vizualization instance in one object
     let visuals = {
         viz_1_1: {
-            data: []
+            data: [],
+            options: { 
+                selector: '#viz_1_1' 
+            }
         },
         viz_1_3: {
-            data: []
+            data: [],
+            options: { 
+                selector: '#viz_1_3' 
+            }
         },
         viz_2_5: {
-            data: []
+            data: [],
+            options: { 
+                selector: '#viz_2_5' 
+            }
         },
     }
 
@@ -36,9 +45,10 @@
             console.log(visuals)
         });
 
+        // only update the visuals if the update flag is true
         if (update == true) {
             Object.keys(visuals).forEach(viz => {
-                if (visuals[viz].hasOwnProperty('viz')){
+                if (visuals[viz].hasOwnProperty('viz')) {
                     visuals[viz].viz.update(visuals[viz].data)
                 }
             })
@@ -56,6 +66,7 @@
     await loadData('en');
 
     document.querySelector('#language').addEventListener('change', async (e) => {
+        // check the language has changed
         if (e.target.value != language) {
             language = e.target.value
             await loadData(language, true)
@@ -66,6 +77,6 @@
     /////// load visuals ///////////
     ////////////////////////////////
 
-    visuals.viz_2_5.viz = barArc(visuals.viz_2_5.data, {selector: '#viz_2_5'})
+    visuals.viz_2_5.viz = barArc(visuals.viz_2_5.data, visuals.viz_2_5.options)
 
 })()
