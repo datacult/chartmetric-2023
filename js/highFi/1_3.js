aef7042bca9dd938bd5b96c26839d831849e67e0
neil-oliver <neiloliverphoto@gmail.com>
2024-01-25 05:08:28 -0500

    Update 1_3.js

--- js/highFi/1_3.js
+++ js/highFi/1_3.js
@@ -1,15 +1,18 @@
-export function Table_1_3(data = [], selector = "vis", platform = "Spotify") {
-  // console.log(data);
- 
-  function draw() {
+export function Table_1_3(data = [], selector = "vis", platform = "Radio") {
+
+  function update(newData = data, newPlatform = platform) {
+    data = newData != null ? newData : data
+    platform = newPlatform
+
+    console.log('table_1_3 update', data, platform);
 
     // Filtering objects based on the 'Platform' property
     const filteredData = data.filter(track => track["PLATFORM"] === platform);
 
-    // console.log(filteredData);
-
     // Sorting filtered array based on 'track_rank' in ascending order
-    const sortedArray = filteredData.sort((a, b) => a.track_rank - b.track_rank);
+    const sortedArray = filteredData.sort((a, b) => a.TRACK_RANK - b.TRACK_RANK);
+
+    console.log(sortedArray)
 
     // Rendering HTML elements to element with selector
     const wrapper = document.getElementById(selector);
@@ -19,13 +22,13 @@ export function Table_1_3(data = [], selector = "vis", platform = "Spotify") {
 
     // Removing each found element
     elementsToRemove.forEach(element => {
-    element.remove();
+      element.remove();
     });
 
     sortedArray.forEach(obj => {
-    const htmlElement = document.createElement('div');
-    htmlElement.className = "box1";
-    htmlElement.innerHTML = `
+      const htmlElement = document.createElement('div');
+      htmlElement.className = "box1";
+      htmlElement.innerHTML = `
         <div class="rank">${obj["TRACK_RANK"]}</div>
         <img class="rank-img" src="${obj["IMAGE_URL"]}" alt="${obj["ARTIST_NAME"]}">
         <div class="track-text">
@@ -33,14 +36,13 @@ export function Table_1_3(data = [], selector = "vis", platform = "Spotify") {
             <div class="subheader"><span class="color-bar"></span>${obj["ARTIST_NAME"]}</div>
         </div>
     `;
-    wrapper.appendChild(htmlElement);
+      wrapper.appendChild(htmlElement);
     });
 
   }
-  function update(data) {
-    draw();
-  }
-  update(data)
+
+  update()
+
   return {
     update: update,
   };
